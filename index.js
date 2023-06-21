a860ef89ea45a2ef27bfd05762feb63e5c7d03b3
emxanuel <ealg0528@gmail.com>
2023-06-20 23:23:02 -0400

    updates

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const server = require('http').Server(app)
 const WebSocketServer = require('websocket').server;
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://192.168.8.147:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
