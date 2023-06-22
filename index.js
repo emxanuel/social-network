692324a01e333421816b4e319828526e81b00271
emxanuel <ealg0528@gmail.com>
2023-06-22 00:11:38 -0400

    updates

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const server = require('http').Server(app)
 const WebSocketServer = require('websocket').server;
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://emxanuel.github.io/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://emxanuel.github.io');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
