7388e492d3a59f4aa5fad78ec60e25849c2ccf0d
emxanuel <ealg0528@gmail.com>
2023-06-22 23:10:29 -0400

    updates

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const http = require('http');
 const server = http.createServer(express)
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://emxanuel.github.io');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
