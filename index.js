f1b31f7d7c6a856f43711a18dc5c29c8bf518666
emxanuel <ealg0528@gmail.com>
2023-06-25 23:13:59 -0400

    updates

--- index.js
+++ index.js
@@ -9,8 +9,13 @@ const http = require('http');
 const server = http.createServer(express)
 const ServerlessHttp = require('serverless-http');
 
+const urls = [
+    'http://localhost:3000',
+    'https://emxanuel.github.io'
+]
+
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', urls[1]);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
