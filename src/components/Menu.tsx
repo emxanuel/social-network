5b1e457ed03608d9f23df20fdbfb9d53fefa06c9
emxanuel <ealg0528@gmail.com>
2023-06-26 16:35:38 -0400

    menu added in navbar

---
+++ src/components/Menu.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import style from '../css/menu.module.css'
+
+const Menu = () => {
+    return (
+        <div id='menu' className={`${style.container}`} >
+            <div className={style.link}>
+                <i className="fa-solid fa-magnifying-glass" />
+                <Link to='/search'>Search friends</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Menu
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,27 +2,43 @@ import { useUserContext } from './UserContext'
 import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import styles from '../css/navbar.module.css'
+import Menu from './Menu'
+import { useState } from 'react'
 
 const Navbar = () => {
     const user = useUserContext()
     const navigate = useNavigate();
+    const [toggle, setToggle] = useState(true);
+
+    const toggleNavbar = () => {
+        let menu = document.getElementById('menu') as HTMLElement;
+        if (toggle){
+            menu.style.display = 'flex'
+        }
+        else{
+            menu.style.display = 'none'
+        }
+    }
     const Logged = () => {
         return (
             <nav className = {styles.navbar}>
                 <div>
-                    <h1>SN</h1>
-                    <p></p>
-                    <p></p>
-                    <p></p>
+                    <h1 className={styles.logo}><Link to='/'>SN</Link></h1>
                 </div>
-                <div>
+                <div className={styles.links}>
                     <p>{user.first_name}</p>
                     {/* <img src={user.profilePicture} alt="" /> */}
                     <p onClick={() => {
                         localStorage.clear()
                         window.location.reload()
-                    }}>Log out</p>
+                    }}>Logout</p>
+                    <i className={`${styles.icon} fa-solid fa-bars`} onClick={async () => {
+                        await setToggle(!toggle)
+                        console.log(!toggle)
+                        toggleNavbar()
+                    }}/>
                 </div>
+                <Menu />
             </nav>
         )
     }
