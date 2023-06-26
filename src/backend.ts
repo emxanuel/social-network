db3e3cf2092e41fa616bb604db2680024fbabd32
emxanuel <ealg0528@gmail.com>
2023-06-25 23:14:36 -0400

    servers updated to new api on render

--- src/backend.ts
+++ src/backend.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-const base = 'https://my-api-rest.azurewebsites.net'
-const wsBase = 'wss://my-api-rest.azurewebsites.net'
+const base = 'https://my-api-rest.onrender.com'
+const wsBase = 'wss://my-api-rest.onrender.com'
 export const server = `${base}/api`
 export const wsServer = `${wsBase}:81`
 export const Axios = axios.create({
