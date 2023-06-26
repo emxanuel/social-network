a61fd10db40369873761711225792a3135342ce1
emxanuel <ealg0528@gmail.com>
2023-06-25 23:50:15 -0400

    server and websocket server in the same port

--- src/backend.ts
+++ src/backend.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 const base = 'https://my-api-rest.onrender.com'
-const wsBase = 'wss://my-api-rest.onrender.com'
+export const wsServer = 'wss://my-api-rest.onrender.com'
 export const server = `${base}/api`
-export const wsServer = `${wsBase}:81`
 export const Axios = axios.create({
     baseURL: server
 })
