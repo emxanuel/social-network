ea17438cf7c7824cf3a5694ce8cbea12bab0c806
emxanuel <ealg0528@gmail.com>
2023-07-11 22:42:53 -0400

    remove unused imports from contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import styles from '../css/contact.module.css'
 import { UserData, useUserContext } from './UserContext';
