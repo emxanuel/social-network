866e562bcd11d10ba7f133554d8d291a85a7487d
emxanuel <ealg0528@gmail.com>
2023-08-08 01:47:40 -0400

    add dark mode to chat skeleton

--- src/components/ChatSkeleton.tsx
+++ src/components/ChatSkeleton.tsx
@@ -1,16 +1,46 @@
 import { Skeleton } from '@mui/material'
 import style from '../css/chat.module.css'
+import { useThemeContext } from './Theme'
 
 const ChatSkeleton = () => {
+    const theme = useThemeContext()
     return (
-        <div className={style.skeleton}>
-            <Skeleton width={'40vw'} height={'30vh'} animation='wave' />
-            <Skeleton width={'30vw'} height={'30vh'} animation='wave' className={style.right}/>
-            <Skeleton width={'20vw'} height={'40vh'} animation='wave' />
-            <Skeleton width={'50vw'} height={'40vh'} animation='wave' />
-            <Skeleton width={'20vw'} height={'40vh'} animation='wave' className={style.right}/>
-            <Skeleton width={'20vw'} height={'60vh'} animation='wave' />
-            <Skeleton width={'20vw'} height={'60vh'} animation='wave' className={style.right}/>
+        <div>
+            {theme === 'dark' ? (
+                <div className={style.skeleton}>
+                    <Skeleton sx={{
+                        bgcolor:'rgb(22,22,22)'
+                    }} width={'40vw'} height={'30vh'} animation='wave' />
+                    <Skeleton sx={{
+                        bgcolor:'rgb(22,22,22)'
+                    }} width={'30vw'} height={'30vh'} animation='wave' className={style.right} />
+                    <Skeleton sx={{
+                        bgcolor:'rgb(22,22,22)'
+                    }} width={'20vw'} height={'40vh'} animation='wave' />
+                    <Skeleton sx={{
+                        bgcolor:'rgb(22,22,22)'
+                    }} width={'50vw'} height={'40vh'} animation='wave' />
+                    <Skeleton sx={{
+                        bgcolor:'rgb(22,22,22)'
+                    }} width={'20vw'} height={'40vh'} animation='wave' className={style.right} />
+                    <Skeleton sx={{
+                        bgcolor:'rgb(22,22,22)'
+                    }} width={'20vw'} height={'60vh'} animation='wave' />
+                    <Skeleton sx={{
+                        bgcolor:'rgb(22,22,22)'
+                    }} width={'20vw'} height={'60vh'} animation='wave' className={style.right} />
+                </div>
+            ) : (
+                <div className={style.skeleton}>
+                    <Skeleton width={'40vw'} height={'30vh'} animation='wave' />
+                    <Skeleton width={'30vw'} height={'30vh'} animation='wave' className={style.right} />
+                    <Skeleton width={'20vw'} height={'40vh'} animation='wave' />
+                    <Skeleton width={'50vw'} height={'40vh'} animation='wave' />
+                    <Skeleton width={'20vw'} height={'40vh'} animation='wave' className={style.right} />
+                    <Skeleton width={'20vw'} height={'60vh'} animation='wave' />
+                    <Skeleton width={'20vw'} height={'60vh'} animation='wave' className={style.right} />
+                </div>
+            )}
         </div>
     )
 }
